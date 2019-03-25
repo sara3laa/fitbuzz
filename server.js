21652c43969da7ddc0c2292706be8f11db86425e
sara3laa <sara3laa.93@gmail.com>
2019-03-25 22:57:38 +0200

    setup stsrt script and heroku port

--- server.js
+++ server.js
@@ -3,4 +3,6 @@ const app = require('./app');
 
 const port = process.env.PORT || 3000;
 const server = http.createServer(app);
-server.listen(port);
+server.listen(port, () => {
+  console.log(`server listen to port ${port}`);
+});
